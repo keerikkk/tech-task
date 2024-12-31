c1d688c0f3735e00903477c79a1de5396643719b
Kyrylo Svir <ksvir17@gmail.com>
2024-12-31 02:46:26 +0200

    feat: add auth form

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,27 @@
 import { Outlet } from "react-router";
 import NavBar from "../components/NavBar";
+import { useAuth } from "../components/AuthProvider";
+import AuthForm from "../components/AuthForm";
+import { Container } from "@mui/material";
 
 const Layout = () => {
+  const { user } = useAuth();
+
   return (
     <>
-      <NavBar />
-      <main>
-        <Outlet />
-      </main>
-      <footer>footer</footer>
+      {user ? (
+        <>
+          <NavBar />
+          <main>
+            <Outlet />
+          </main>
+          <footer>footer</footer>
+        </>
+      ) : (
+        <Container sx={{ width: "100vw" }}>
+          <AuthForm />
+        </Container>
+      )}
     </>
   );
 };
