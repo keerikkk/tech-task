c0aefc6c32548503c2da47fd7047c4b13aacef2c
Kyrylo Svir <ksvir17@gmail.com>
2025-01-03 16:45:07 +0200

    refactor: change navbar styles & add logout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,10 +7,11 @@ import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Button } from "@mui/material";
 import { useLocation } from "react-router";
 import { navItems } from "../data/navigationLinks";
+import { useAuth } from "./AuthProvider";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   alignItems: "center",
-  paddingTop: theme.spacing(1),
+  paddingTop: 0,
   paddingBottom: theme.spacing(2),
   display: "grid",
   gridTemplateColumns: "1fr 3fr 1fr",
@@ -18,6 +19,7 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
 }));
 
 export default function NavBar() {
+  const { handleLogout } = useAuth();
   const theme = useTheme();
   const location = useLocation();
 
@@ -31,7 +33,9 @@ export default function NavBar() {
             variant="contained"
             sx={{
               justifySelf: "center",
+              marginTop: theme.spacing(1),
             }}
+            onClick={handleLogout}
           >
             Закрити
           </Button>
@@ -41,6 +45,7 @@ export default function NavBar() {
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
+              height: "100%",
               fontSize: "1.25rem",
               fontWeight: "bold",
               color: theme.palette.common.black,
@@ -59,6 +64,7 @@ export default function NavBar() {
               height: "2rem",
               justifySelf: "center",
               border: "1px solid #fff",
+              marginTop: theme.spacing(1),
               borderRadius: "50%",
               "&:hover": {
                 border: "1px solid rgba(255,255,255,0.5)",
