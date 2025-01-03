f67c98804c067def3b07cea2675d2c7b0dcaefdd
Kyrylo Svir <ksvir17@gmail.com>
2025-01-03 16:13:09 +0200

    refactor: remove unnecessary imports

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,24 +1,12 @@
-import * as React from "react";
 import { useNavigate } from "react-router";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import { navItems } from "../data/navigationLinks";
-import { useAuth } from "./AuthProvider";
-
-const drawerWidth = 240;
 
 export default function DrawerAppBar() {
-  const { handleLogout } = useAuth();
   const navigate = useNavigate();
 
   return (
